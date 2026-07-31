9127baf2626ff21860ed7270b5ee8a6c42a752c5
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2026-07-31 18:03:17 -0400

    Configure Oracle Entity Framework Core data access

--- AstroTrack.Api/Program.cs
+++ AstroTrack.Api/Program.cs
@@ -1,8 +1,20 @@
+using AstroTrack.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("OracleDb");
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'OracleDb' was not found. Configure it with appsettings.json, appsettings.Development.json, environment variables, or .NET user secrets.");
+}
+
+builder.Services.AddDbContext<AstroTrackDbContext>(options =>
+    options.UseOracle(connectionString));
 
 builder.Services.AddCors(options =>
 {
